805ae1f97666b5e5ebecf372e60ec9a2f2376083
Avedis-Neehus <32306001+Avedis-Neehus@users.noreply.github.com>
2017-11-12 12:59:27 +0100

    Changes to button and label to make key_input mouse_sensitive

--- mathee/utility.py
+++ mathee/utility.py
@@ -34,14 +34,14 @@ class label(event_queue):
     keys = { pig.K_1 : 1, pig.K_2 : 2, pig.K_3 : 3 , pig.K_4 : 4 , pig.K_5 :5 , pig.K_6 : 6, pig.K_7 : 7
             , pig.K_8 : 8 ,pig.K_9 : 9, pig.K_0 : 0}
     
-    values = []
+    
     
     def __init__(self, x,y, size = 50):
         
         self.x = x
         self.y = y
         self.size = size
-      
+        self.values = []
         
     def fill_values(self):
         
@@ -61,33 +61,40 @@ class label(event_queue):
                     except:
                         #index out of bounds
                         pass
-                    
     @property               
     def number(self):
         num = 0
         for i,lit in enumerate(self.values):
             num += lit*10**(len(self.values) -i-1)
         return num     
-                                   
+    
+                        
     def display(self):
         num = ''.join(str(i) for i in self.values)        
-        message_display(num, self.x, self.y, self.size)
+        button(self.x, self.y, 40,40, num, no_click_func = self.fill_values, size = self.size)
         
-    @classmethod    
-    def delete(cls):
-        cls.values[:] = []
-       
+        
+     
+    def delete(self):
+        self.values[:] = []
+        
+    @staticmethod 
+    def delete_all(inst):
+        
+        [label.delete() for label in inst]
   
-def button(x,y,w,h, text, func = lambda : None, size = 30):
+def button(x,y,w,h, text = '', func = lambda : None, no_click_func = lambda : None , size = 30):
     #wenn innerhalb button
     mouse = pig.mouse.get_pos()
     
     
     if x+w > mouse[0] > x and y+h > mouse[1] > y:
+        
         pig.draw.rect(gameDisplay, black,(x,y,w,h))
+        message_display(text,x+(w/2),y+(h/2),size)
+        no_click_func()
         
-        #wenn gedrückt
-        
+        #wenn gedrückt        
         for event in event_queue.events:
             if event.type == pig.MOUSEBUTTONDOWN:
                 
@@ -97,6 +104,7 @@ def button(x,y,w,h, text, func = lambda : None, size = 30):
         #zeig den button
         pig.draw.rect(gameDisplay, red,(x,y,w,h))
         message_display(text,x+(w/2),y+(h/2),size)        
+      
 
 
     
